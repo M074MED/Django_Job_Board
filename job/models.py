1ac9e08b8361c8ef9e4ad54f9e2356cd3dfe6195
M074MED <mhmedmhmed1234951@gmail.com>
2021-09-24 15:25:44 +0200

    add job models

--- job/models.py
+++ job/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Job(models.Model):
+    job_types = [('Full-time', 'Full-time'), ('Part-time', 'Part-time')]
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    job_type = models.CharField(max_length=20, default='Full-time', choices=job_types)
+    published_on = models.DateTimeField(auto_now=True)
+    vacancy = models.IntegerField(default=1)
+    salary = models.DecimalField(decimal_places=2, max_digits=6, null=True, blank=True)
+    experience = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.title
--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
+    'job',
+    'blog',
+    'accounts',
+    'contact',
 ]
 
 MIDDLEWARE = [
@@ -73,10 +78,21 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Django_Job_Board',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
